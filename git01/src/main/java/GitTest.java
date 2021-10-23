84edac1565e716e478f6a0ac737a585d888db15d
wang <1433076629@qq.com>
2021-10-23 20:46:34 +0800

    idea third commit3

--- git01/src/main/java/GitTest.java
+++ git01/src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("helllo Git!!!");
         System.out.println("helllo Git2!!!");
+        System.out.println("helllo Git2!!!");
     }
 }
